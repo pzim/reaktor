5baaabfa7becdee70a7117104c7d29b2ad672547
Tim Hughes <tim.hughes@lmax.com>
2016-01-13 14:36:13 +0000

    change to use real tmpdir

--- spec/puppetfile_spec.rb
+++ spec/puppetfile_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'r10k/puppetfile'
 require 'fileutils'
+require 'tmpdir'
 
 describe Reaktor::R10K::Puppetfile do
   let(:logger)  { double('logger').as_null_object }
@@ -9,48 +10,37 @@ describe Reaktor::R10K::Puppetfile do
   let(:mod) { ('testmod1') }
   let(:mod2) { ('testmod2') }
   let(:repo_name) { ('myproject-testmod2') }
-  
+
   let(:now) { Time.now.strftime('%Y%m%d%H%M%S%L') }
   #let(:puppetfile_orig) { File.new(read_fixture("Puppetfile")) }
   let(:puppetfile_orig) { File.new("spec/unit/fixtures/Puppetfile") }
-  let(:git_work_dir) { FileUtils.mkdir_p('/var/tmp/puppetfile_test') }
+  let(:git_work_dir) { Dir.mktmpdir('rspec') }
+
 
   subject { described_class.new(branch, mod, logger) }
 
   it 'should update mod testmod1 ref to be branch name' do
-    subject.git_work_dir = git_work_dir[0]
-    FileUtils.cp("spec/unit/fixtures/Puppetfile", "#{git_work_dir[0]}/Puppetfile")
+    subject.git_work_dir = git_work_dir
+    FileUtils.cp("spec/unit/fixtures/Puppetfile", "#{git_work_dir}/Puppetfile")
     contents = subject.update_module_ref(mod, branch)
     expect(contents).to include(branch)
+    FileUtils.remove_entry_secure git_work_dir
   end
 
   it 'should update mod testmod2 ref to be branch2 name' do
-    subject.git_work_dir = git_work_dir[0]
-    FileUtils.cp("spec/unit/fixtures/Puppetfile", "#{git_work_dir[0]}/Puppetfile")
+    subject.git_work_dir = git_work_dir
+    FileUtils.cp("spec/unit/fixtures/Puppetfile", "#{git_work_dir}/Puppetfile")
     contents = subject.update_module_ref(mod2, branch2)
     expect(contents).to include(branch2)
-  end
-  
-  it 'should retrieve testmod2 as module name' do
-    subject.git_work_dir = git_work_dir[0]
-    FileUtils.cp("spec/unit/fixtures/Puppetfile", "#{git_work_dir[0]}/Puppetfile")
-    contents = subject.get_module_name(repo_name)
-    expect(contents).to eq(mod2)    
+    FileUtils.remove_entry_secure git_work_dir
   end
 
-##
-#  it 'created should be true' do
-#    created = subject.created
-#    expect(created).to eql (true)
-#  end
-#
-#  it 'deleted should be false' do
-#    deleted = subject.deleted
-#    expect(deleted).to eql (false)
-#  end
-#
-#  it 'repo name should be webhook_test_two' do
-#    repo_name = subject.repo_name
-#    expect(repo_name).to eql ('webhook_test_two')
-#  end
+  it 'should retrieve testmod2 as module name' do
+    subject.git_work_dir = git_work_dir
+    FileUtils.cp("spec/unit/fixtures/Puppetfile", "#{git_work_dir}/Puppetfile")
+    contents = subject.get_module_name(repo_name)
+    expect(contents).to eq(mod2)
+    FileUtils.remove_entry_secure git_work_dir
+  end
+
 end
