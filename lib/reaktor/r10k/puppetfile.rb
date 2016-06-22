97a56acbbdef7df637ea6d117fce5c206e80791b
Walter Kleynscheldt <walter.kleynscheldt@hetzner.co.za>
2016-06-22 17:44:09 +0200

    We now force the use of the proper module name syntax as defined by modules metadata docs

--- lib/reaktor/r10k/puppetfile.rb
+++ lib/reaktor/r10k/puppetfile.rb
@@ -28,7 +28,7 @@ module Reaktor
       # @param repo_name - The repo name assiociated with the module
       def get_module_name(repo_name)
         pfile = loadFile
-        regex = /mod ["'](\w*(-\w*)?)["'],\s*$\n^(\s*):git\s*=>\s*["'].*#{repo_name}(\.git)?["'],+(\s*)(:tag|:ref)\s*=>\s*['"](\w+|\w+\.\d+\.\d+)['"]$/
+        regex = /mod ["']\w*-(\w*)["'],\s*$\n^(\s*):git\s*=>\s*["'].*#{repo_name}(\.git)?["'],+(\s*)(:tag|:ref)\s*=>\s*['"](\w+|\w+\.\d+\.\d+)['"]$/
           new_contents = pfile.match(regex)
         if new_contents
           module_name = new_contents[1]
