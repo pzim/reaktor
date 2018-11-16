8bedc8a98e811fcd35c74d19b08f78b7df77b7ac
Walter Kleynscheldt <walter.kleynscheldt@hetzner.co.za>
2018-11-16 11:21:19 +0200

    Add new notifier

--- lib/reaktor/notification/notifier.rb
+++ lib/reaktor/notification/notifier.rb
@@ -1,4 +1,3 @@
-require 'observer'
 require 'singleton'
 require 'slack-notifier'
 require 'yaml'
@@ -6,13 +5,21 @@ require 'yaml'
 module Reaktor
 module Notification
   class Notifier
+    include Singleton
+
+    attr_accessor :notification
 
     def initialize
-      @config = YAML::load(File.open('./config/notifiers.yml'))
+      @config = YAML.load_file(('config/notifiers.yml'))
+      @logger ||= Logger.new(STDOUT, Logger::INFO)
+      @logger.info("NOTIFICATION initialised")
     end
 
-    def self.send_message(message,room:'default')
+    def self.send_message(message,room='default')
+      @logger.info("NOTIFIER room: #{room}")
       webhook_uri = @config['notifiers'][room]
+      @logger.info("NOTIFIER webhook_uri #{webhook_uri}")
+      @logger.info("NOTIFIER delivering message via slack")
       notifier = Slack::Notifier.new(webhook_uri)
       notifier.ping message
     end
