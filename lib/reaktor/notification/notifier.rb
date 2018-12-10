c94ed5099beac1c04ecbd7d90c1cab512a044cc3
Walter Kleynscheldt <walter.kleynscheldt@hetzner.co.za>
2018-12-10 16:00:10 +0200

    Add more error handling in notifier.rb

--- lib/reaktor/notification/notifier.rb
+++ lib/reaktor/notification/notifier.rb
@@ -12,16 +12,21 @@ module Notification
     def initialize
       @config = YAML.load_file(('config/notifiers.yml'))
       @logger ||= Logger.new(STDOUT, Logger::INFO)
+      @logger.info("NOTIFICATION initialised")
     end
 
     def send_message(message,room='default')
+      return if message.to_s.strip.empty?
       webhook_uri = @config['notifiers'][room]
       if webhook_uri.nil?
         @logger.error("Webhook_uri not found for room #{room}.")
         raise "Webhook_uri not found for room #{room}."
       end
+      @logger.info("#{webhook_uri} #{message}")
       notifier = Slack::Notifier.new(webhook_uri)
       notifier.ping message.chomp
+    rescue => e
+      @logger.error("Exception caught: #{e.class} #{e.message} #{e.backtrace}")
     end
 
   end
